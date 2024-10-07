7c4becdffc13dbfc2b00fcf581689ce4c774f679
dogghoodie <dogghoodie@gmail.com>
2024-10-07 00:18:37 -0500

    Added invalid command, and made the panel a little more something to look for my eyes.

--- pkg/panel/panel.go
+++ pkg/panel/panel.go
@@ -1,7 +1,6 @@
 package panel
 
 import (
-	"Sovereign/pkg/visuals"
 	"bufio"
 	"fmt"
 	"os"
@@ -10,18 +9,23 @@ import (
 
 // Function creates our main client panel
 func CreatePanel() {
-	fmt.Println(visuals.Colors.RED + "Sovereign Panel." + visuals.Colors.ANSI_RESET)
+	// Temporary design just for now
+	fmt.Println("***********************")
+	fmt.Println("* Client Panel Switch *")
+	fmt.Println("***********************")
+	fmt.Println()
+	fmt.Println("commands: ping, connect, quit")
 
 	// Create the run loop
 	for {
+		// Text prompt formatting
+		fmt.Print("$ ")
 		// Initialize reader
 		reader := bufio.NewReader(os.Stdin)
-
 		// Initialize command reader as next string
 		command, err := reader.ReadString('\n')
 		// Clean up any white space after command
 		command = strings.ReplaceAll(command, "\n", "")
-
 		switch command {
 		// Just a test
 		case "ping":
@@ -37,6 +41,9 @@ func CreatePanel() {
 			fmt.Println()
 			// Returns out of loop
 			return
+		default:
+			fmt.Println("Invalid command.")
+			fmt.Println()
 		}
 
 		// If there is an error, print
