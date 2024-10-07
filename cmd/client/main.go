57363f0e3bdd11fdde319296fb0b1353fdc1f2b1
dogghoodie <dogghoodie@gmail.com>
2024-10-07 00:15:01 -0500

    Implemented logo.go into main.go
    New main function first clears the screen, draws the logo and waits for
    2 seconds, then clears the screen again and moves onto the panel.
    Imported pkg/visuals, fmt, and time.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "Sovereign/pkg/panel"
+import (
+	"Sovereign/pkg/panel"
+	"Sovereign/pkg/visuals"
+	"fmt"
+	"time"
+)
 
 func main() {
-	// initialize client interface potentially rename to gocui terms
+	// Clear the screen
+	fmt.Print("\033[H\033[2J")
+
+	// Print Sovereign ascii logo to the screen
+	visuals.Draw()
+	time.Sleep(2000 * time.Millisecond)
+
+	// Clear the screen
+	fmt.Print("\033[H\033[2J")
+
+	// Initialize client interface potentially rename to gocui terms
 	panel.CreatePanel()
 }
