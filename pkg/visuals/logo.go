0641ffe477123575d8dd1157795742fbc43fe1f9
dogghoodie <dogghoodie@gmail.com>
2024-10-07 00:17:17 -0500

    Created logo.go to print our logo.txt file with our red color value.

--- pkg/visuals/logo.go
+++ pkg/visuals/logo.go
@@ -1,1 +1,34 @@
 package visuals
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Draw() {
+	// Define relative path to the logo.txt file
+	sourceFile := "resources/logo.txt"
+
+	// Open the file
+	file, err := os.Open(sourceFile)
+	if err != nil {
+		fmt.Println("Error openig file: ", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	fmt.Println()
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(Colors.RED + line + Colors.ANSI_RESET)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file: ", err)
+	}
+
+}
