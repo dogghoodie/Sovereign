e349156bfff0371de0b3efae6fef98497343c293
WildLegolas <jakemilliken24@gmail.com>
2024-10-08 21:15:06 -0500

    more animation progress

--- pkg/visuals/launch_animation.go
+++ pkg/visuals/launch_animation.go
@@ -1,207 +1,143 @@
 package visuals
 
-package main
-
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
-// Speed adjusters for each group of characters
-var (
-	switchSpeedSlash    = 100 * time.Millisecond // Speed for / and 7
-	switchSpeedBackslash = 150 * time.Millisecond // Speed for \ and >
-	switchSpeedPipe     = 200 * time.Millisecond // Speed for |, [, ], I, l, etc.
-	switchSpeedDollar   = 250 * time.Millisecond // Speed for $, s, h, etc.
-	letterAppearance    = 0.5  // 50% chance of a letter block appearing
-	negativeChance      = 0.2  // 20% chance of a letter block coming in as negative
-)
+// Define character of interest
+var charsOfInterest = []rune{'$', '/', '\\', '|', '_', ' '}
 
-// The Sovereign logo, with each letter represented by an array of strings (blocks)
-var logoLetters = [][]string{
-	{ // "S"
-		"  /$$$$$$  ",
-		" /$$__  $$ ",
-		"| $$  \\ $$ ",
-		"| $$$$$$$$ ",
-		"| $$__  $$ ",
-		"| $$  | $$ ",
-		"|  $$$$$$/ ",
-		" \\______/  ",
-	},
-	{ // "o"
-		"   /$$$$$$  ",
-		"  /$$__  $$ ",
-		" | $$  \\__/ ",
-		" |  $$$$$$  ",
-		"  \\____  $$ ",
-		"  /$$  \\ $$ ",
-		" |  $$$$$$/ ",
-		"  \\______/  ",
-	},
-	{ // "v"
-		"  /$$    /$$ ",
-		" |  $$  /$$/ ",
-		"  \\  $$/$$/  ",
-		"   \\  $$$/   ",
-		"    \\  $/    ",
-		"     | $$    ",
-		"     | $$    ",
-		"     |__/    ",
-	},
-	{ // "e"
-		"  /$$$$$$$$ ",
-		" | $$_____/ ",
-		" | $$       ",
-		" | $$$$$$   ",
-		" | $$__  $$ ",
-		" | $$  \\ $$ ",
-		" |  $$$$$$/ ",
-		"  \\______/  ",
-	},
-	{ // "r"
-		"  /$$$$$$   ",
-		" /$$__  $$  ",
-		"| $$  \\ $$  ",
-		"| $$$$$$$$  ",
-		"| $$__  $$  ",
-		"| $$  | $$  ",
-		"|  $$$$$$$  ",
-		" \\_______/  ",
-	},
-	{ // "e"
-		"  /$$$$$$$$ ",
-		" | $$_____/ ",
-		" | $$       ",
-		" | $$$$$$   ",
-		" | $$__  $$ ",
-		" | $$  \\ $$ ",
-		" |  $$$$$$/ ",
-		"  \\______/  ",
-	},
-	{ // "i"
-		"    /$$   ",
-		"   | $$   ",
-		"   | $$   ",
-		"   | $$   ",
-		"   | $$   ",
-		"   | $$   ",
-		"   | $$$$$",
-		"   \\_____/ ",
-	},
-	{ // "g"
-		"   /$$$$$$  ",
-		"  /$$__  $$ ",
-		" | $$  \\__/ ",
-		" |  $$$$$$  ",
-		"  \\____  $$ ",
-		"  /$$  \\ $$ ",
-		" |  $$$$$$/ ",
-		"  \\______/  ",
-	},
-	{ // "n"
-		"  /$$   /$$ ",
-		" | $$  / $$ ",
-		" |  $$/ $$  ",
-		"  \\  $$$$   ",
-		"   \\  $$/   ",
-		"    | $$    ",
-		"    | $$    ",
-		"    |__/    ",
-	},
+// Define blocks and their coordinates
+var blockCoordinates = map[string][2][2]int{
+	"S": {{1, 1}, {10, 12}},
+	"o": {{3, 13}, {10, 22}},
+	"v": {{3, 23}, {10, 33}},
+	"e1": {{3, 33}, {10, 42}},
+	"r": {{3, 42}, {10, 52}},
+	"e2": {{3, 53}, {10, 62}},
+	"i": {{1, 63}, {10, 66}},
+	"g": {{3, 67}, {13, 76}},
+	"n": {{3, 77}, {10, 86}},
 }
 
-// Substitution mappings for switching characters
-var switchMap = map[rune][]rune{
-	'/':  {'7'},
-	'\\': {'>'},
-	'|':  {'[', ']', 'I', 'l', ';', ':', '!', 'i'},
-	'$':  {'s', 'h', 'b', 'V', 'D', 'g', 'w', '#', 'k', '*', 'M', 'm', '`', '.', '%', '~', '"', '\''},
+// Fill characters
+var fillChars = []rune("qwertyughlodkrasd")
+
+// Function to read the text file and populate a 2D array
+func readFileInto2DArray(filePath string) [][]rune {
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return nil
+	}
+	defer file.Close()
+
+	var array2D [][]rune
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := []rune(scanner.Text())
+		array2D = append(array2D, line)
+	}
+	return array2D
 }
 
-// Substitution timing for each group of characters
-func substituteWithDelay(c rune, delay time.Duration) rune {
+// Get coordinates for characters of interest
+func getCoordinates(array2D [][]rune, charsOfInterest []rune) map[rune][][]int {
+	coordMap := make(map[rune][][]int)
+	for row := range array2D {
+		for col := range array2D[row] {
+			char := array2D[row][col]
+			for _, targetChar := range charsOfInterest {
+				if char == targetChar {
+					coordMap[targetChar] = append(coordMap[targetChar], []int{row + 1, col + 1}) // 1-indexed
+				}
+			}
+		}
+	}
+	return coordMap
+}
+
+// Get the coordinates for each block's characters of interest
+func getBlockCoordinates(blockLetter string, coordMap map[rune][][]int) map[rune][][]int {
+	blockCoord := blockCoordinates[blockLetter]
+	blockCoordsMap := make(map[rune][][]int)
+
+	for char, coords := range coordMap {
+		for _, coord := range coords {
+			if coord[0] >= blockCoord[0][0] && coord[0] <= blockCoord[1][0] && // Check row
+				coord[1] >= blockCoord[0][1] && coord[1] <= blockCoord[1][1] { // Check column
+				blockCoordsMap[char] = append(blockCoordsMap[char], coord)
+			}
+		}
+	}
+	return blockCoordsMap
+}
+
+// Print character to terminal at specified coordinates
+func printAtCoordinate(row, col int, char rune) {
+	fmt.Printf("\033[%d;%dH%s", row, col, string(char)) // Move cursor and print
+}
+
+// Function to animate a block positively by filling chars randomly
+func positiveLetter(blockLetter string, coordMap map[rune][][]int, delay time.Duration) {
+	timeTotal := 2*time.Second - delay
 	time.Sleep(delay)
-	return substituteChar(c)
-}
 
-// Substitution logic for characters
-func substituteChar(c rune) rune {
-	if subs, exists := switchMap[c]; exists {
-		return subs[rand.Intn(len(subs))]
-	}
-	return c
-}
-
-// Randomize appearance of individual letter blocks with optional negative effects
-func randomizeLetter(letter []string) []string {
-	result := make([]string, len(letter))
-	for i, line := range letter {
-		var newLine strings.Builder
-		for _, char := range line {
-			// Apply different delays based on character type
-			switch char {
-			case '/', '7':
-				char = substituteWithDelay(char, switchSpeedSlash)
-			case '\\', '>':
-				char = substituteWithDelay(char, switchSpeedBackslash)
-			case '|', '[', ']', 'I', 'l', ';', ':', '!', 'i':
-				char = substituteWithDelay(char, switchSpeedPipe)
-			case '$', 's', 'h', 'b', 'V', 'D', 'g', 'w', '#', 'k', '*', 'M', 'm', '`', '.', '%', '~', '"', '\'':
-				char = substituteWithDelay(char, switchSpeedDollar)
-			}
-
-			// Random chance for negative effect
-			if rand.Float64() < negativeChance {
-				newLine.WriteRune(' ')
-			} else {
-				newLine.WriteRune(char)
-			}
+	startTime := time.Now()
+	for time.Since(startTime) < timeTotal {
+		for _, coord := range append(coordMap['$'], coordMap['/']...) { // Combine coord lists of interest
+			row, col := coord[0], coord[1]
+			randomChar := fillChars[rand.Intn(len(fillChars))]
+			printAtCoordinate(row, col, randomChar)
+			time.Sleep(10 * time.Millisecond)
 		}
-		result[i] = newLine.String()
 	}
-	return result
 }
 
-// Render the ASCII logo with randomized effects for each letter block
-func renderLogo() {
-	for row := 0; row < len(logoLetters[0]); row++ { // Loop through each row of the letters
-		for _, letter := range logoLetters {
-			if rand.Float64() < letterAppearance {
-				randomizedLetter := randomizeLetter(letter)
-				fmt.Print(randomizedLetter[row])
-			} else {
-				fmt.Print(strings.Repeat(" ", len(letter[row]))) // Empty space for missing letter
-			}
-			fmt.Print("  ") // Extra spacing between letters
+// Function to animate a block negatively by filling space coordinates
+func negativeLetter(blockLetter string, coordMap map[rune][][]int, delay time.Duration) {
+	timeTotal := 2*time.Second - delay
+	time.Sleep(delay)
+
+	startTime := time.Now()
+	for time.Since(startTime) < timeTotal {
+		for _, coord := range coordMap[' '] { // Use space coordinates
+			row, col := coord[0], coord[1]
+			randomChar := fillChars[rand.Intn(len(fillChars))]
+			printAtCoordinate(row, col, randomChar)
+			time.Sleep(10 * time.Millisecond)
 		}
-		fmt.Println() // Move to next line
 	}
 }
 
-// Continuously animate the logo in the terminal
-func animateLogo() {
-	for {
-		renderLogo()
-		time.Sleep(100 * time.Millisecond) // Adjust frame rate here
+// Main function to process blocks and animate them
+func animateBlocks(blockList []string, coordMap map[rune][][]int) {
+	for _, blockLetter := range blockList {
+		blockCoords := getBlockCoordinates(blockLetter, coordMap)
+		delay := time.Duration(rand.Intn(450)+50) * time.Millisecond
+		if rand.Float64() > 0.3 {
+			positiveLetter(blockLetter, blockCoords, delay)
+		} else {
+			negativeLetter(blockLetter, blockCoords, delay)
+		}
 	}
 }
 
-func animate_call() {
+func Animate_call() {
 	rand.Seed(time.Now().UnixNano())
+	// Read the ASCII art file into a 2D array
+	array2D := readFileInto2DArray("resources/logo.txt")
 
-	// Customize animation parameters here
-	switchSpeedSlash = 80 * time.Millisecond
-	switchSpeedBackslash = 120 * time.Millisecond
-	switchSpeedPipe = 180 * time.Millisecond
-	switchSpeedDollar = 220 * time.Millisecond
-	letterAppearance = 0.7  // 70% chance of letters appearing
-	negativeChance = 0.3    // 30% chance of negative effect
+	// Get all coordinates of characters of interest
+	coordMap := getCoordinates(array2D, charsOfInterest)
 
-	// Start the ASCII animation
-	animateLogo()
+	// List of blocks to animate
+	blockList := []string{"S", "o", "v", "e1", "r", "e2", "i", "g", "n"}
+
+	// Start the animation
+	animateBlocks(blockList, coordMap)
 }
-
-
-//TODO: Settle on an animation idea then implement with python frames script.
