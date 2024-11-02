bc72906b8937a80651a444aec890cf1206465668
dogghoodie <dogghoodie@gmail.com>
2024-11-01 23:45:21 -0500

    Adjusted naming and added keybinds for settings-popup and close popup. Close popup is broken right now.

--- pkg/gui/keybindings.go
+++ pkg/gui/keybindings.go
@@ -19,21 +19,35 @@ func setKeyBindings(g *gocui.Gui) error {
 
 	// Bind j to move down.
 	if err := g.SetKeybinding("", 'j', gocui.ModNone, func(gui *gocui.Gui, v *gocui.View) error {
-		return Move(gui, v, "down")
+		return Move(g, v, "down")
 	}); err != nil {
 		return err
 	}
 
 	// Bind k to move up.
 	if err := g.SetKeybinding("", 'k', gocui.ModNone, func(gui *gocui.Gui, v *gocui.View) error {
-		return Move(gui, v, "up")
+		return Move(g, v, "up")
 	}); err != nil {
 		return err
 	}
 
 	// Bind l to move right.
 	if err := g.SetKeybinding("", 'l', gocui.ModNone, func(gui *gocui.Gui, v *gocui.View) error {
-		return Move(gui, v, "right")
+		return Move(g, v, "right")
+	}); err != nil {
+		return err
+	}
+
+	// Bind s to open settings.
+	if err := g.SetKeybinding("", 's', gocui.ModNone, func(gui *gocui.Gui, v *gocui.View) error {
+		return ShowSettings(g, v)
+	}); err != nil {
+		return err
+	}
+
+	// Bind ESC to close popups.
+	if err := g.SetKeybinding("", gocui.KeyEsc, gocui.ModNone, func(gui *gocui.Gui, v *gocui.View) error {
+		return ClosePopup(g, v)
 	}); err != nil {
 		return err
 	}
