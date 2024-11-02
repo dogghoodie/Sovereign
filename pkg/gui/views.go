c5aec6e95bb34f83ac2fdf3f417e0c2cfb5c9060
dogghoodie <dogghoodie@gmail.com>
2024-11-01 23:47:12 -0500

    Removed original settings view and created the popup view for settings instead && adjusted navigation map.

---
+++ pkg/gui/views.go
@@ -0,0 +1,92 @@
+package gui
+
+import (
+	"fmt"
+	"github.com/jroimartin/gocui"
+)
+
+// Navigation map for tabs.
+var viewNavigation = map[string]map[string]string{
+	"chat":        {"left": "connections", "down": "message"},
+	"message":     {"left": "connections", "up": "chat"},
+	"connections": {"right": "message"},
+}
+
+func InitializeViews(g *gocui.Gui) error {
+	if err := SetupMainViews(g); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Defines the layout for the GUI.
+func SetupMainViews(g *gocui.Gui) error {
+	// Get max x,y values for gui grid.
+	maxX, maxY := g.Size()
+
+	// Conncetions tab for viewing chat partners.
+	if connectionsTab, err := g.SetView("connections", 0, 0, maxX/2-20, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		// Testing layout shit.
+		connectionsTab.Title = "Connections"
+		fmt.Fprintln(connectionsTab)
+	}
+
+	// Chat tab for viewing messages.
+	if chatboxTab, err := g.SetView("chat", maxX/2-18, 0, maxX-1, maxY-6); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		chatboxTab.Title = "Chat"
+		fmt.Fprintln(chatboxTab)
+		fmt.Fprintln(chatboxTab, " ")
+	}
+
+	// Message tab for writing to the chat.
+	if messageTab, err := g.SetView("message", maxX/2-18, maxY-5, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		messageTab.Title = "Message"
+
+		// TODO: Make messageTab editable, save input somewhere as 'message'
+		// reading this for now:
+		// https://gist.github.com/jroimartin/3b2e943a3811d795e0718b4a95b89bec
+
+		messageTab.Editable = true
+		messageTab.Wrap = true
+		fmt.Fprintln(messageTab)
+		fmt.Fprintln(messageTab, ">")
+	}
+
+	return nil
+}
+
+func ShowSettings(g *gocui.Gui, v *gocui.View) error {
+	// Get max x,y values for gui grid.
+	maxX, maxY := g.Size()
+
+	// Popup dimensions
+	width, height := 70, 20
+
+	x0, y0 := (maxX-width)/2, (maxY-height)/2
+	x1, y1 := x0+width, y0+height
+
+	// Settings tab for setting seed and connection settings.
+	if settingsPopup, err := g.SetView("popup_settings", x0, y0, x1, y1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		// Testing layout.
+		settingsPopup.Title = "Settings"
+		fmt.Fprintln(settingsPopup)
+	}
+
+	if _, err := g.SetCurrentView("popup_settings"); err != nil {
+		return err
+	}
+
+	return nil
+}
